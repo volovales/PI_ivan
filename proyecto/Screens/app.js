a0bbb1c4e1b2b2b9ec4c61de830ec1a837969dd1
volovales <124051396@upq.edu.mx>
2025-11-27 13:38:23 -0600

    para comensar a trabajr
    .

---
+++ proyecto/Screens/app.js
@@ -0,0 +1,14 @@
+import { Text, StyleSheet, View } from 'react-native'
+import React, { Component } from 'react'
+
+export default class app extends Component {
+  render() {
+    return (
+      <View>
+        <Text>app</Text>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({})
