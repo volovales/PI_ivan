fc85c1e673e0fbda58bed5b7e7f01a3ae484f593
Emiliano-SB <124051706@upq.edu.mx>
2025-11-27 21:50:09 -0600

    Update App.js
    Primeras interfaces

--- proyecto/App.js
+++ proyecto/App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import Inicio from "./Screens/InicioScreen";
+import Hemorragia from "./Screens/HemorragiasScreen";
+import Rcp from "./Screens/RcpScreen";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Inicio">
+        <Stack.Screen name="Inicio" component={Inicio} />
+        <Stack.Screen name="Hemorragias" component={Hemorragia} />
+        <Stack.Screen name="Rcp" component={Rcp}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
