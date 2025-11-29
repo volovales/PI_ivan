b6558e5723f4714187cfce6152d5741895cdd7d2
Emiliano-SB <124051706@upq.edu.mx>
2025-11-28 22:20:27 -0600

    Update App.js
    Todas las interfaces de la información

--- proyecto/App.js
+++ proyecto/App.js
@@ -4,6 +4,10 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Inicio from "./Screens/InicioScreen";
 import Hemorragia from "./Screens/HemorragiasScreen";
 import Rcp from "./Screens/RcpScreen";
+import Fractura from "./Screens/FracturasScreen";
+import Esguince from "./Screens/EsguincesScreen";
+import Atragantamiento from "./Screens/AtragantamientoScreen";
+import Quemadura from "./Screens/QuemadurasScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -14,6 +18,10 @@ export default function App() {
         <Stack.Screen name="Inicio" component={Inicio} />
         <Stack.Screen name="Hemorragias" component={Hemorragia} />
         <Stack.Screen name="Rcp" component={Rcp}/>
+        <Stack.Screen name="Fractura" component={Fractura}/>
+        <Stack.Screen name="Esguince" component={Esguince}/>
+        <Stack.Screen name="Atragantamiento" component={Atragantamiento}/>
+        <Stack.Screen name="Quemadura" component={Quemadura}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
